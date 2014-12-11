046e1677e27010f48fddb06884085e839be7ef19
Charles Bunton <buntonc@advisory.com>
2014-12-11 17:28:03 -0500

    refactor SleepMode.java to interrupt thread properly

--- src/android/SleepMode.java
+++ src/android/SleepMode.java
@@ -13,27 +13,30 @@ public class SleepMode extends CordovaPlugin {
 	protected static final String TAG = "SleepModePlugin";
 
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		final callbackContext = callbackContext;
+	public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		if (action.equals("sleepApp")) {
-			try {				
-				Intent i = new Intent(Intent.ACTION_MAIN);
-                i.addCategory(Intent.CATEGORY_HOME);
-                this.cordova.getActivity().startActivity(i);
-				
-			} catch (Exception e) {
-				LOG.e(TAG, "Exception occurred: ".concat(e.getMessage()));
-				return false;
-			}
-			this.cordova.getActivity().runOnUiThread(new Runnable() {
-     			public void run() {
-         			callbackContext.success(); 
-     			}
- 			});
+			this.initialize(callbackContext, args);
 			return true;
+		} else {
+			LOG.e(TAG, "Called invalid action: "+ action);
+			return false; 
 		}
-		LOG.e(TAG, "Called invalid action: "+ action);
-		return false;  
+	}
+
+	private void initialize(final CallbackContext callbackContext, final JSONArray args) {
+		cordova.getActivity().runOnUiThread(new Runnable () {
+			public void run () {
+				try {
+					Intent i = new Intent(Intent.ACTION_MAIN);
+                	i.addCategory(Intent.CATEGORY_HOME);
+                	cordova.getActivity().startActivity(i);
+                	callbackContext.success();
+				} catch (Exception e) {
+					LOG.e(TAG, "Exception occurred: ".concat(e.getMessage()));
+					callbackContext.error("Exception");
+				}
+			}
+		});
 	}
 
 }
